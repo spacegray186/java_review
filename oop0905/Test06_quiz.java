d5d3402919650c889f567fcd8459712dde640c29
spacegray186 <109955351+spacegray186@users.noreply.github.com>
2022-09-07 09:42:08 +0900

    Day029_220905

--- oop0905/Test06_quiz.java
+++ oop0905/Test06_quiz.java
@@ -2,20 +2,20 @@ package oop0905;
 
 public class Test06_quiz {
 
-	public static void main(String[] args) {
-		// 연습문제
-		// 참조 : 지폐개수 구하는 문제
-		/*
-			1년 : 365.2422일
-			출력결과 : 365일 5시간 48분 46초
-			
-			1분		: 60초
-			1시간	: 60분*60초 = 3600초
-			1일		: 24시간*3600초 = 86400초
-			1년		: 365.2422*86400 = 31,556,926.08
-		*/
-		
-		double year=365.2422;
+    public static void main(String[] args) {
+        //연습문제
+        //참조 : 지폐개수 구하는 문제
+        /*
+           1년 : 365.2422일
+           출력결과 : 365일 5시간 48분 46초  
+           
+           1분 : 60초
+           1시간 : 60분*60초       = 3600초
+           1일   : 24시간*3600초   = 86400초
+           1년   : 365.2422*86400 = 31,556,926.08        
+        */
+        
+        double year=365.2422;
         int total=(int)(year*86400);//31556926
         //System.out.println(total);
         
@@ -33,8 +33,6 @@ public class Test06_quiz {
         int second=total%60;
         System.out.println(second+"초");
         
-		
 
-	} // main() end
-
-} // class end
+    }//main() end
+}//class end
