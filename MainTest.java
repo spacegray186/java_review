8f4d57442d48aab52c743ac41155b2e92d0836e1
spacegray186 <109955351+spacegray186@users.noreply.github.com>
2022-09-07 18:11:52 +0900

    Day031_220907

---
+++ MainTest.java
@@ -0,0 +1,15 @@
+
+public class MainTest {
+
+	public static void main(String[] args) {	//argument string
+		//oop0907.Test06_main.java 참조
+		//주의사항 : 한글이 포함되어 있으면 컴파일시 에러나는 경우가 발생합니다
+		//			주석을 포함한 한글을 삭제후 테스트하시기 바랍니다
+		
+		for(int i=0; i<args.length; i++) {
+			System.out.println(args[i]);
+		}//for end
+
+	}//main() end
+
+}//class end
